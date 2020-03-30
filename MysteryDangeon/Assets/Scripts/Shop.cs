63d2a3d6a9b37118ee451206047aa2851ba83974
staff <krims12345@gmail.com>
2020-03-30 15:26:11 +0300

    add shop

---
+++ MysteryDangeon/Assets/Scripts/Shop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        var rooms = FindObjectsOfType<Shop>();
+        foreach (Shop room in rooms)
+        {
+            room.GetComponent<SpriteRenderer>().color = new Color32(130,130,130,255);
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
