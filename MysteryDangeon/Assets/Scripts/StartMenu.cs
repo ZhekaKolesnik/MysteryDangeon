929beafe591990402531eb91d59f0d02d178bbad
staff <krims12345@gmail.com>
2020-03-31 18:44:39 +0300

    добавил меню магаза

---
+++ MysteryDangeon/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] private RectTransform menuContainer;
+
+    [Header("Smooth")]
+    [SerializeField] private bool smooth;
+    [SerializeField] private float smoothSpeed = 0.1f;
+    [SerializeField] private Vector3 desiredPosition;
+
+    private Vector3[] menuPositions;
+
+    private void Start()
+    {
+        //получить текущую позицию
+        menuPositions = new Vector3[menuContainer.childCount];
+        Vector3 halfScreen = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        for (int i = 0; i < menuPositions.Length; i++)
+        {
+            menuPositions[i] = menuContainer.GetChild(i).position - halfScreen;
+        }
+    }
+
+    private void Update()
+    {
+        if (smooth)
+        {
+            menuContainer.anchoredPosition = Vector3.Lerp(menuContainer.anchoredPosition, desiredPosition, smoothSpeed);
+        }
+        else
+        {
+            menuContainer.anchoredPosition = desiredPosition;
+        }
+    }
+
+    public void MoveMenu(int id)
+    {
+        desiredPosition = -menuPositions[id];
+    }
+}
